4ab798b8c50087faee56b32a5d06dce8c75eb794
Lucas Elisei <lucas@elisei.ch>
2025-08-07 19:14:55 +0200

    Replace tuono index page with ours

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,47 +1,12 @@
 import type { JSX } from 'react'
-import type { TuonoRouteProps } from 'tuono'
 
-interface IndexProps {
-  subtitle: string
-}
-
-export default function IndexPage({
-  data,
-  isLoading,
-}: TuonoRouteProps<IndexProps>): JSX.Element {
-  if (isLoading) {
-    return <h1>Loading...</h1>
-  }
-
+export default function IndexPage(): JSX.Element {
   return (
     <>
-      <header className="header">
-        <a href="https://crates.io/crates/tuono" target="_blank">
-          Crates
-        </a>
-        <a href="https://www.npmjs.com/package/tuono" target="_blank">
-          Npm
-        </a>
-      </header>
-      <div className="title-wrap">
-        <h1 className="title">
-          TU<span>O</span>NO
+      <div className='bg-background mx-auto'>
+        <h1 className='text-6xl font-bold text-center'>
+          Baby Signs
         </h1>
-        <div className="logo">
-          <img src="rust.svg" className="rust" />
-          <img src="react.svg" className="react" />
-        </div>
-      </div>
-      <div className="subtitle-wrap">
-        <p className="subtitle">{data?.subtitle}</p>
-        <a
-          href="https://github.com/tuono-labs/tuono"
-          target="_blank"
-          className="button"
-          type="button"
-        >
-          Github
-        </a>
       </div>
     </>
   )
