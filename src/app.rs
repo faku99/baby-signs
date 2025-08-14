3a9825c4ac342fab4cd9fd32b5a486ab2e6ebf2a
Lucas Elisei <lucas@elisei.ch>
2025-08-14 15:20:08 +0200

    Implement logging

--- src/app.rs
+++ src/app.rs
@@ -1,3 +1,5 @@
+use tracing::Level;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use tuono_app::models::session::SessionManager;
 
 #[derive(Clone)]
@@ -6,6 +8,16 @@ pub struct ApplicationState {
 }
 
 pub fn main() -> ApplicationState {
+    // Initialize logger with tracing.
+    // We want to filter out logs from other crates so stdout doesn't get polluted.
+    let filter = tracing_subscriber::filter::Targets::new()
+        .with_target("tuono_app", Level::DEBUG)
+        .with_default(Level::INFO);
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(filter)
+        .init();
+
     let session_manager = SessionManager::new();
 
     ApplicationState { session_manager }
