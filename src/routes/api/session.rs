58174e7f9a304785a1c89da6204d384b8ad8ce3b
Lucas Elisei <lucas@elisei.ch>
2025-08-17 09:46:41 +0200

    Create /api/session GET route

---
+++ src/routes/api/session.rs
@@ -0,0 +1,20 @@
+use serde::Serialize;
+use serde_json::{json, Value};
+use tuono_app::{cookie::BSCookie, models::session::SessionManager};
+use tuono_lib::{axum::Json, Request, Type};
+
+#[derive(Serialize, Type)]
+struct ApiSessionResponse {
+    name: String,
+}
+
+#[tuono_lib::api(GET)]
+async fn session_random_sign(req: Request, session_manager: SessionManager) -> Json<Value> {
+    if let Some(cookie) = BSCookie::from_request(&req) {
+        let _session = session_manager.get_session(&cookie.session_id);
+    }
+
+    Json(json!(ApiSessionResponse {
+        name: "hello".to_string()
+    }))
+}
