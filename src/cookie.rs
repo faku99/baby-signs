77adc1354f665d198af5107dc3ba59a1a100b9cc
Lucas Elisei <lucas@elisei.ch>
2025-08-19 17:49:52 +0200

    Add 'iat' and 'exp' claims to BSCookie

--- src/cookie.rs
+++ src/cookie.rs
@@ -1,12 +1,12 @@
-use jsonwebtoken::{decode, encode, DecodingKey, EncodingKey, Header, Validation};
+use jsonwebtoken::{Algorithm, DecodingKey, EncodingKey, Header, Validation, decode, encode};
 use serde::{Deserialize, Serialize};
 use tuono_lib::{
-    cookie::{Cookie, CookieJar},
     Request,
+    cookie::{Cookie, CookieJar},
 };
 
 const COOKIE_NAME: &str = "bs_session"; // // TODO: Use env variable instead
-const KEY: &[u8; 6] = b"secret";    // TODO: Use env variable instead
+const KEY: &[u8; 6] = b"secret"; // TODO: Use env variable instead
 
 #[derive(Debug)]
 pub struct BSCookie {
@@ -15,15 +15,22 @@ pub struct BSCookie {
 
 #[derive(Debug, Deserialize, Serialize)]
 struct Claims {
+    iat: i64,
+    exp: i64,
     session_id: String,
 }
 
 impl BSCookie {
     pub fn new(session_id: String) -> BSCookie {
+        let iat = chrono::Local::now();
+        let exp = iat + chrono::Duration::days(90);
+
         BSCookie {
             claims: Claims {
+                iat: iat.timestamp_millis(),
+                exp: exp.timestamp_millis(),
                 session_id,
-            }
+            },
         }
     }
 
@@ -35,7 +42,11 @@ impl BSCookie {
         let jar = CookieJar::from_headers(&req.headers);
         let value = jar.get(COOKIE_NAME).map(|c| c.value().to_owned())?;
 
-        let data = decode::<Claims>(&value, &DecodingKey::from_secret(KEY), &Validation::default());
+        let data = decode::<Claims>(
+            &value,
+            &DecodingKey::from_secret(KEY),
+            &Validation::new(Algorithm::HS256),
+        );
         if let Err(data) = &data {
             tracing::error!("{}", data);
             return None;
@@ -47,7 +58,11 @@ impl BSCookie {
     }
 
     fn as_jwt(&self) -> Option<String> {
-        let token = encode(&Header::default(), &self.claims, &EncodingKey::from_secret(KEY));
+        let token = encode(
+            &Header::default(),
+            &self.claims,
+            &EncodingKey::from_secret(KEY),
+        );
         if let Err(token) = &token {
             tracing::error!("{}", token);
             return None;
@@ -59,11 +74,13 @@ impl BSCookie {
     pub fn as_cookie(&self) -> Option<Cookie> {
         let jwt = self.as_jwt()?;
 
-        Some(Cookie::build((COOKIE_NAME, jwt))
-            .domain("localhost") // TODO: Use env variable instead
-            .path("/")
-            .secure(false)
-            .http_only(true)
-            .build())
+        Some(
+            Cookie::build((COOKIE_NAME, jwt))
+                .domain("localhost") // TODO: Use env variable instead
+                .path("/")
+                .secure(false)
+                .http_only(true)
+                .build(),
+        )
     }
 }
